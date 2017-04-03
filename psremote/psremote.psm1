d707ff021cca7b991aa44010c5dc8090735bf63a
jake watkins <jake.watkins@gmail.com>
2017-04-03 15:49:39 -0500

    Update psremote.psm1

--- psremote/psremote.psm1
+++ psremote/psremote.psm1
@@ -7,7 +7,7 @@
 function SetupAdminCredentials
 {
 	$userName = "administrator"
-	$password = convertto-securestring -string "Ph0ne.Mouse.Paper.Card" -AsPlainText -force
+	$password = convertto-securestring -string "not my password" -AsPlainText -force
 	$global:credential = new-object -typename System.Management.Automation.PSCredential -argumentList $userName, $password
 }
 export-modulemember -function SetupAdminCredentials 
